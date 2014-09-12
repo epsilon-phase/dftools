5e718b566e2e267af755df12b98ba33c1478eda4
jaked122 <jaked122>
2014-09-12 18:23:09 +0000

    Attempted to write an automatic minimum allowable error discovery function

--- shape.cpp
+++ shape.cpp
@@ -2,12 +2,23 @@
 Shape::Shape(){
 }
 Shape::~Shape(){}
+double discoverInterval(const std::vector<Point> &i){
+    double sum=0;
+    for(int c=1;c<i.size();c++){
+        sum+=Distance(i[c],i[c-1]);
+    }
+    return sum/i.size();
+}
 void Shape::intersectionPoint(const Shape& i,int &n,std::vector<Point> &output)const{
     std::vector<Point> p1=i.getPoints();
     std::vector<Point> p2=getPoints();
+    //find the average allowable error
+    double error=discoverInterval(p1);
+    error+=discoverInterval(p2);
+    error/=2.0;
     for(auto c:p1){
         for(auto d:p2){
-            if(compareWithError(Distance(c,d),0,0.001)){
+            if(compareWithError(Distance(c,d),0,error)){
                 //average together the error
                 output.push_back(Point((c.x+d.x)/2,(c.y+d.y)/2));
             }
